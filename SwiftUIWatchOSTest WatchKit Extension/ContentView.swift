be2947c276fa5db272aacf082616aac96a8bfaa5
Tamás Sengel <tamas.sengel@toptal.com>
2020-12-06 19:35:57 +0100

    Initial Commit

---
+++ SwiftUIWatchOSTest WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWatchOSTest WatchKit Extension
+//
+//  Created by Sengel Tamás on 12/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIWatchOSTest WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  SwiftUIWatchOSTest WatchKit Extension
+//
+//  Created by Sengel Tamás on 12/6/20.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
